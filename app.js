fdd9712753570777131b5037e57b5ec492fba413
Courtney Fay <fay.courtney@gmail.com>
2017-09-25 18:02:11 -0600

    adds court as a charaacter and begins move function

--- app.js
+++ app.js
@@ -50,38 +50,72 @@ document.addEventListener('DOMContentLoaded', function() {
 		// constructor function to create character "Court" 
 		function Character() {
 			this.name = name;
-			//this.element = document.createElement("")
+			this.element = document.createElement('div');
+			//this.class = element.classList.add('court');
 		};
 
 		// prototype method for the Character object: move()
 		Character.prototype = {
+
+			// creates the character as a div on top of cell 40 to start
+			create: function() {
+				let specialCell = tdArray[40];
+				specialCell.append(this.element);
+				this.element.classList.add('character');
+			},
+
+			// moves the character either up, right, down or left depending on which arrow key is pressed
 			move: function() {
-				console.log('Im moving');
+				
+				// TODO: first add function that blocks move (checks to see if that cell is a wall block)
+
+				switch (event.key) {
+					case 'ArrowUp':
+						console.log('move up');
+						let thisCell = this.element.parentElement;
+						let cellNum = parseInt(thisCell.getAttribute('data-num'));
+						cellNum + 9;
+						console.log(cellNum);
+						this.element.append()
+
+						/* let specialCell = tdArray[40];
+						specialCell.append(this.element); */
+						// move Court div up 9 numbers in the td Array
+						break;
+					case 'ArrowRight':
+						console.log('move right');
+						// move Court div right 1 number in the td Array
+						break;
+					case 'ArrowDown':
+						console.log('move down');
+						// move Court div down 9 numbers in the td Array
+						break;
+					case 'ArrowLeft':
+						console.log('move left');
+						// move Court div left 1 number in the td Array
+						break;
+				} 
 			}
 		};
 
+		// creates Court as the main character and puts her on the map
 		let court = new Character("Court");
+		court.create();
 
 		// turns on event listeners for the arrow keys to help move Court
-		//document.addEventListener('keypress',keypressListener);
-		console.log(event.key);
-		console.log(event);
+		document.addEventListener('keydown',keypressListener);
 
-		// function keypressListener() {
-		 	
-		// };
-    /*if (event.key === 's' || event.key === 'S') { 
-      players[0].rideDown();
-
-    } else if (event.key === 'k' || event.key === 'K') {
-      players[1].rideDown();
-    } else {
-      console.log('you pressed the wrong key');
-      console.log(event);
-      console.log(event.key);
-    }*/
+		function keypressListener() {
+			if (event.key === 'ArrowUp' || event.key === 'ArrowRight' || event.key === 'ArrowDown' || event.key === 'ArrowLeft') { 
+      	court.move();	
+      }
+		};
    };
-   
-	createBoard();
-	createCharacter();
+
+  function startGame() {
+  	createBoard();
+		createCharacter();
+  };
+
+  startGame();
 });
