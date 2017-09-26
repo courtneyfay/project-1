c3f0ff38f961f5c7876b346194334c84e65b942d
Courtney Fay <fay.courtney@gmail.com>
2017-09-26 14:14:59 -0600

    starts a key collector function

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	// global variables
 	let gamePage = document.getElementById('game-page');
-	let tdArray = [];
-	let hubTitles = ['Timer', 'Chips Left', 'Keys'];
-	let hubContent = [100,5];
+	let hubContentArray = [100,5];
 	let wallBlocksArray = [2,3,4,8,11,13,17,18,19,20,22,23,24,25,26,27,33,36,42,45,51,52,53,54,60,63,69,72,73,74,75,76,77,78,79,80];
 	let keyArray = [0,7,32,62];
-	let keyDoors = [19,23,69];
+	let keyDoorsArray = [19,23,69];
+	let tdArray = [];
+	let keysCollectedArray = [];
 
 	function createBoard() {
 
@@ -19,24 +19,54 @@ document.addEventListener('DOMContentLoaded', function() {
 		rightColumn.classList.add('game-sidebar');
 		gamePage.append(rightColumn);
 
-		//TODO create a key table and add it to the hubContent array 
+		// creates a key table with 1 row and 4 columns, and pushes it into the hubcontentarray
 		let keyTable = document.createElement('table');
 		keyTable.classList.add('key-table');
-		hubContent.push(keyTable);
-		console.log(hubContent);
+		hubContentArray.push(keyTable);
+
+		for (let i = 0; i < 1; i++) {
+			let tr2 = document.createElement('tr');
+			tr2.classList.add('table-row');
+			keyTable.append(tr2);
+
+			for (let i = 0; i < 4; i++) {
+				let td2 = document.createElement('td');
+				td2.classList.add('key-locker');
+				tr2.append(td2);
+			}
+		}
+
+		/*// create 9 new table rows and append them to the new table
+		for (let i = 0; i < 9; i++) {
+			let tr = document.createElement('tr');
+			tr.classList.add('table-row');
+			table.append(tr);
+
+			//create 9 new cells and append to the corresponding table row
+			for (let i = 0; i < 9; i++) {
+				let td = document.createElement('td');
+				tdArray.push(td);
+				tr.append(td);
+			}
+		}*/
 		
 		// adds divs to the right aside and adds classes to style them
+		let hubTitlesArray = ['Timer', 'Chips Left', 'Keys'];
 		for (let i = 0; i < 3; i++) {
 			// creates hub titles
 			let hubDiv = document.createElement('div');
 			hubDiv.classList.add('sidebar-div');
-			hubDiv.innerHTML = hubTitles[i];
+			hubDiv.innerHTML = hubTitlesArray[i];
 			rightColumn.append(hubDiv);
 			
 			// creates hub content 
 			let hubSubDiv = document.createElement('div');
 			hubSubDiv.classList.add('sidebar-sub-div');
-			hubSubDiv.innerHTML = hubContent[i];
+			if (typeof(hubContentArray[i]) === 'object') {
+				hubSubDiv.append(keyTable);
+			} else {
+				hubSubDiv.innerHTML = hubContentArray[i];
+			}
 			hubDiv.append(hubSubDiv);
 		}
 		
@@ -68,15 +98,24 @@ document.addEventListener('DOMContentLoaded', function() {
 			tdArray[i].setAttribute('data-num', i);
 		}
 
-		//if td array data-num value is equal to one in the wall blocks array, then add a class of wall-block
-		// TODO: add class of key-door for the keyDoor array
+		//if td array data-num value is equal to one in the wall blocks array, 
 		for (let i = 0; i < tdArray.length; i++) {
 			let tdNum = parseInt(tdArray[i].getAttribute('data-num'));
+
+			// then add a class of wall-block
 			for (let i = 0; i < wallBlocksArray.length; i++) {
 				let wbNum = wallBlocksArray[i];
 				if (tdNum === wbNum) { 
 					tdArray[tdNum].classList.add('wall-block');
 				} 
+			}
+			// adds a class of key-door if in the keyDoors array
+			for (let i = 0; i < keyDoorsArray.length; i++) {
+				let kdNum = keyDoorsArray[i];
+				if (tdNum === kdNum) {
+					tdArray[tdNum].classList.remove('wall-block');
+					tdArray[tdNum].classList.add('key-door');
+				}
 			}
 		}
 	};
@@ -89,7 +128,7 @@ document.addEventListener('DOMContentLoaded', function() {
 			this.element = document.createElement('div');
 		};
 
-		// prototype method for the Character object: create(), checkDirection(), move(), isWall() 
+		// prototype method for the Character object: create(), checkDirection(), move(), isWall(), isDoor(), collect(), isKey()
 		Character.prototype = {
 
 			// creates the character as a div on top of cell 40 to start
@@ -129,16 +168,18 @@ document.addEventListener('DOMContentLoaded', function() {
 				cellNum = cellNum + direction;
 				let newCell = tdArray[cellNum];
 
-				//check to see whether it is a wall first!
-				if(this.isWall(newCell)) {
+				// first check to see whether it is a wall or a keydoor, then look for stuff to collect and move!
+				if (this.isWall(newCell)) {
 					return;
+				} else if (this.isDoor(newCell)) {
+					console.log('next step is to check to see if chip has collected the key!');
 				} else {
-					// TODO check to see if you need to collect anything, create new collect() function!!
+					this.collect(newCell);
 					newCell.append(this.element);
 				}
 			},
 
-			// if cellNum is equal to any of the items in wallBlocksArray return true, else false
+			// if cellNum has a class of wall-block, return true, else false
 			isWall: function(newCell) {
 				if (newCell.classList.value === 'wall-block') {
 					return true;
@@ -147,12 +188,39 @@ document.addEventListener('DOMContentLoaded', function() {
 				}
 			},
 
-			collect: function() {
-				console.log('Im checking for something to collect');
-				// first make the key div disappear from the map
-				// next make the key div appear on the right hand side in the 'key locker'
-				// finally, push the key into an array of keys collected?
-			}
+			// if cellNum has a class of key-door, return true, else false
+			isDoor: function(newCell) {
+				if (newCell.classList.value === 'key-door') {
+					return true;
+				} else {
+					return false;
+				}
+			},
+
+			collect: function(newCell) {
+				if (this.isKey(newCell) /*|| this.isChip(newCell)*/) {
+					
+					// first make the key div disappear from the map 
+					newCell.classList.remove('key');
+				
+					// TODO: next make the key div appear on the right hand side in the 'key locker'
+
+					// finally, push the key into an array of keys collected?
+
+
+				} else {
+					console.log('naht a key');
+				};
+			},
+
+			isKey: function(newCell) {
+				if (newCell.classList.value === 'key') {
+					return true;
+				} else {
+					return false;
+				}
+			},
+
 		};
 
 		// creates Court as the main character and puts her on the map
@@ -206,8 +274,8 @@ document.addEventListener('DOMContentLoaded', function() {
   };
 
   function startTimer() {
-  	console.log('I started the timer at 100 seconds');
-  	console.log(hubContent[0]); //timer value
+  	// console.log('I started the timer at 100 seconds');
+  	// console.log(hubContentArray[0]); //timer value
 
   	/*function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
